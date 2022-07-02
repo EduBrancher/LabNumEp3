a1e1fa9d10192b1cc45c486b254e90bc8b4604c1
edu <EduBrancher>
2022-07-02 14:35:33 -0300

    terminado

--- parte1.c
+++ parte1.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <math.h>
+
+double random1[100000000];
+double random2[100000000];
 
 void print_evaluation(double x, double fx, char* func){
     printf("O valor do polinomio para %s avaliado em %.4lf foi %.4lf.\n", func, x, fx);
+}
+
+double randomUniform(){
+    return (double)rand() / (double)RAND_MAX;
 }
 
 double evaluate_newton_polynomial(double x, double* xarr, int length, double* coefs){
@@ -84,7 +92,76 @@ double integrate_trapezoidal(double start, double end, double h, int length, dou
     return area;
 }
 
+double integrateSin(int n){
+    double integral = 0.0;
+    for (int i = 1; i <= n; i++){
+        double rnum = randomUniform();
+        integral = integral + sin(rnum);
+    }
+    integral = integral / n;
+    return integral;
+}
+
+double gx3(double arg){
+    return (4*arg + 3); 
+}
+
+double integrateXcube(int n){
+    double integral = 0.0;
+    for (int i = 1; i <= n; i++){
+        double rnum = randomUniform();
+        integral = integral + 4*gx3(rnum)*gx3(rnum)*gx3(rnum);
+    }
+    integral = integral/n;
+    return integral;
+}
+
+double integrateExp0_1(int n){
+    double integral = 0.0;
+    for (int i = 1; i <= n; i++){
+        double rnum = randomUniform();
+        integral = integral + exp(-rnum);
+    }
+    integral = integral/n;
+    return integral;
+}
+
+double integrateExpln(int n){
+    double integral = 0.0;
+    for (int i = 1; i <= n; i++){
+        double rnum = randomUniform();
+        integral = integral + (1/(1-rnum))*exp(log(1.0 - rnum) - 1.0);
+    }
+    integral = integral/n;
+    return integral;
+}
+
+double approximatePi(int n){
+    double piaprox = 0.0;
+    //generate n independent from a uniform variable
+    srand(time(NULL)+1);
+    for (int i = 0; i < n; i++){
+        random1[i] = randomUniform();
+    }
+    //reseed to create another independent uniform variable
+    srand(time(NULL)+1007);
+    //take n measurements
+    for (int i = 0; i < n; i++){
+        random2[i] = randomUniform();
+    }
+    for (int i = 0; i < n; i++){
+        if (random1[i]*random1[i] + random2[i]*random2[i] < 1.0 ){
+            piaprox++;
+        }
+    }
+    piaprox = piaprox/n;
+    piaprox = piaprox * 4;
+    return piaprox;
+}
+
 int main(){
+    //use time as seed
+    srand(time(NULL));
     //initialize stuff
     int length = 7;
     double xarr[] = {0.0, 5.0, 10.0, 15.0, 20.0, 25.0, 30.0};
@@ -128,5 +205,32 @@ int main(){
         area = integrate_trapezoidal(0.0, 30.0, h, length, coefs, xarr);
         printf("A area obtida foi %.4lf.\n", area);
         h = h/2;
+    }
+    for (int i = 1; i <= 100; i++){
+        printf("%lf\n", randomUniform(i));
+    }
+    int N = 10;
+    for (int i = 0; i < 7; i++){
+        double seno = integrateSin(N);
+        printf("Integral de seno com n = %d: %lf\n", N, seno);
+        N = N*10;
+    }
+    N = 10;
+    for (int i = 0; i < 7; i++){
+        double x3 = integrateXcube(N);
+        printf("Integral de x^3 com n = %d: %lf\n", N, x3);
+        N = N*10;
+    }
+    N = 10;
+    for (int i = 0; i < 7; i++){
+        double expintegral = integrateExp0_1(N) + integrateExpln(N);
+        printf("Integral de e^x com n = %d: %lf\n", N, expintegral);
+        N = N*10;
+    }
+    N = 10;
+    for (int i = 0; i < 7; i++){
+        double piaprox = approximatePi(N);
+        printf("Aproximacao de pi com n = %d: %lf\n", N, piaprox);
+        N = N*10;
     }
 }
